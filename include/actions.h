a0a7c19366f13f6bb8fc48e8f24f2cc9f4d53e06
kedarui <98135875+listlessneko@users.noreply.github.com>
2026-04-04 16:05:51 -0700

    i have no clue what i am doing

--- include/actions.h
+++ include/actions.h
@@ -7,29 +7,67 @@
 #define CMD_USE "use"
 #define CMD_QUIT "quit"
 
-typedef enum CommandKind {
-  CMD_KIND_EXAMINE,
-  CMD_KIND_GO,
-  CMD_KIND_PICK_UP,
-  CMD_KIND_DROP,
-  CMD_KIND_USE,
-  CMD_KIND_QUIT,
-  CMD_KIND_UNKNOWN
-} CommandKind;
+typedef enum MenuKind {
+  MENU_KIND_MAIN,
+  MENU_KIND_EXPLORE,
+  MENU_KIND_EXAMINE,
+  MENU_KIND_FIGHT,
+  MENU_KIND_EXAMINE_AREA,
+  MENU_KIND_EXAMINE_INVENTORY,
+  MENU_KIND_EXAMINE_MAP,
+  MENU_KIND_EXAMINE_STATS,
+  MENU_KIND_UNKNOWN
+} MenuKind;
+
+typedef enum SelectionKind {
+  SELECT_KIND_QUIT,
+  SELECT_KIND_GO,
+  SELECT_KIND_PICK_UP,
+  SELECT_KIND_USE,
+  SELECT_KIND_DROP,
+  SELECT_KIND_THROW_AWAY,
+  SELECT_KIND_MAP,
+  SELECT_KIND_STATS,
+  SELECT_KIND_UNKNOWN
+} SelectionKind;
+
+typedef struct MenuOptions {
+  union {
+    MenuKind menu;
+    SelectionKind selection;
+  } display_type;
+  char *name;
+  char *description;
+} MenuOptions;
+
+typedef struct Selections {
+  SelectionKind selection_kind;
+  char *name;
+  char *description;
+} Selections;
+
+typedef union MenuData {
+  MenuOptions menu;
+  Selections selection;
+  Area *area;
+  Item *item;
+} MenuData;
 
 typedef struct ParsedCommand {
-  CommandKind kind;
+  SelectionKind kind;
   char target[32];
 } ParsedCommand;
 
-typedef struct MenuOptions {
-  CommandKind command_kind;
-  EntityKind entity_kind;
+typedef struct DisplayOptions {
+  union {
+    MenuKind menu;
+    SelectionKind selection;
+  } display;
   int count;
-  void *data[];
-} MenuOptions;
+  MenuData *data[];
+} DisplayOptions;
 
-MenuOptions *parse_menu_input(Player *player, CommandKind command, EntityKind target_entity_kind, EntityKind from_entity_kind);
+DisplayOptions *parse_menu_input(Player *player, MenuKind menu_kind);
 void display_menu(MenuOptions *options);
 ParsedCommand parse_input(const char *input);
 int explore(Player *player);
--- src/actions.c
+++ src/actions.c
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <stdlib.h>
 #include <string.h>
 #include "entities.h"
@@ -5,10 +7,74 @@
 #include "actions.h"
 #include "game.h"
 #include "inventory.h"
-#include "items.h"
-#include "rooms.h"
+#include "item.h"
+#include "area.h"
 #include "terminal.h"
 #include "utils.h"
+
+MenuOptions main_menu_options[] = {
+  {
+    .display_type = { MENU_KIND_EXPLORE },
+    .name = "Explore",
+    .description = "Continue your adventure.",
+  },
+  {
+    .display_type = { MENU_KIND_EXAMINE },
+    .name = "Examine",
+    .description = "Examine your surroundings and self.",
+  },
+  {
+    .display_type = { MENU_KIND_FIGHT },
+    .name = "Fight",
+    .description = "Take care of the vagabond.",
+  },
+  {
+    .display_type = { SELECT_KIND_QUIT },
+    .name = "Quit",
+    .description = "Quit the adventure.",
+  },
+};
+
+MenuOptions examine_menu_options[] ={
+  {
+    .display_type = { MENU_KIND_EXAMINE_AREA },
+    .name = "Area",
+    .description = "View your surroundings.",
+  },
+  {
+    .display_type = { MENU_KIND_EXAMINE_INVENTORY },
+    .name = "Inventory",
+    .description = "View your inventory.",
+  },
+  {
+    .display_type = { MENU_KIND_EXAMINE_MAP },
+    .name = "Map",
+    .description = "Study your surroundings.",
+  },
+  {
+    .display_type = { MENU_KIND_EXAMINE_STATS },
+    .name = "Stats",
+    .description = "See how far you've come.",
+  }
+};
+
+DisplayOptions *parse_menu_input(Player *player, MenuKind menu_kind) {
+  if (player == NULL || menu_kind == MENU_KIND_UNKNOWN) {
+    return NULL;
+  }
+  
+  int count = 0;
+
+  switch (menu_kind) {
+    case MENU_KIND_MAIN: {
+      count = 3;
+      if (player->current_area->enemy != NULL) {
+        count++;
+      }
+    case MENU_KIND_EXAMINE: {
+    }
+  }
+}
 
 ParsedCommand parse_input(const char *input) {
   ParsedCommand output = {
@@ -35,7 +101,7 @@ int explore(Player *player) {
 
   int choice_int = atoi(choice);
   if (choice_int >= 1 && choice_int <= 4) {
-    explore_room(player, choice_int);
+    explore_area(player, choice_int);
   } else if (strcmp(choice, "quit") == 0) {
     print_text(PRINT_NORMAL5, "You open your eyes and realize it was just a dream.\n");
     return 1;
@@ -45,17 +111,17 @@ int explore(Player *player) {
   return 0;
 }
 
-int drop_item(Player *player, Item *item, Room *room) {
-  if (player == NULL || item == NULL || room == NULL) {
+int drop_item(Player *player, Item *item, Area *area) {
+  if (player == NULL || item == NULL || area == NULL) {
     return DROP_ITEM_INVALID;
   }
 
   int removed_from_inventory = remove_item_from_player_inventory(player, item);
   if (removed_from_inventory == ITEM_REMOVE_FROM_INVENTORY_SUCCESS) {
-    int added_to_room = add_item_to_room(room, item);
-    if (added_to_room == ITEM_ADD_TO_ROOM_SUCCESS) {
+    int added_to_area = add_item_to_area(area, item);
+    if (added_to_area == ITEM_ADD_TO_AREA_SUCCESS) {
       return DROP_ITEM_SUCCESS;
-    } else if (added_to_room == ROOM_ITEMS_FULL) {
+    } else if (added_to_area == AREA_ITEMS_FULL) {
       destroy_item(item);
       return DROP_ITEM_FULL;
     }
@@ -63,16 +129,16 @@ int drop_item(Player *player, Item *item, Room *room) {
   return DROP_ITEM_ERROR;
 }
 
-int pick_up_item(Player *player, Item *item, Room *room) {
-  if (player == NULL || item == NULL || room == NULL) {
+int pick_up_item(Player *player, Item *item, Area *area) {
+  if (player == NULL || item == NULL || area == NULL) {
     return PICK_UP_ITEM_INVALID;
   }
 
-  int removed_from_room = remove_item_from_room(room, item);
-  if (removed_from_room == ITEM_REMOVE_FROM_ROOM_SUCCESS) {
+  int removed_from_area = remove_item_from_area(area, item);
+  if (removed_from_area == ITEM_REMOVE_FROM_AREA_SUCCESS) {
     int added_to_inventory = add_item_to_player_inventory(player, item);
     if (added_to_inventory == INVENTORY_ITEMS_FULL) {
-      add_item_to_room(room, item);
+      add_item_to_area(area, item);
       return PICK_UP_ITEM_FULL;
     } else if (added_to_inventory == ITEM_ADD_TO_INVENTORY_SUCCESS) {
       return PICK_UP_ITEM_SUCCESS;
