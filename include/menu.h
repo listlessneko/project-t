6b03e382b28677697193f06481194cacca7bce9b
kedarui <98135875+listlessneko@users.noreply.github.com>
2026-04-06 23:39:27 -0700

    Added definitions and began work on menu node builder, custom menu memory reallocation, and parsing player choice

--- include/menu.h
+++ include/menu.h
@@ -2,13 +2,19 @@
 
 typedef enum MenuNodeKind {
   MENU_NODE_MAIN,
+  MENU_NODE_BASE_MENU,
   MENU_NODE_EXPLORE,
+  MENU_NODE_EXPLORE_ROOM,
   MENU_NODE_EXAMINE,
   MENU_NODE_FIGHT,
   MENU_NODE_EXAMINE_ROOM,
+  MENU_NODE_EXAMINE_ROOM_ITEM,
   MENU_NODE_EXAMINE_INVENTORY,
+  MENU_NODE_EXAMINE_INVENTORY_ITEM,
   MENU_NODE_EXAMINE_MAP,
   MENU_NODE_EXAMINE_STATS,
+  MENU_NODE_BACK,
+  MENU_NODE_QUIT,
 } MenuNodeKind;
 
 typedef enum MenuKind {
@@ -18,16 +24,28 @@ typedef enum MenuKind {
   MENU_FIGHT,
 } MenuKind;
 
+typedef enum ActionKind {
+  ACTION_BACK,
+  ACTION_QUIT,
+} ActionKind;
+
 typedef struct BaseMenu {
   MenuKind menu_kind;
-  char *name[32];
-  char *description[256];
+  char name[32];
+  char description[256];
 } BaseMenu;
+
+typedef struct BaseAction {
+  ActionKind action_kind;
+  char name[32];
+  char description[256];
+} BaseAction;
 
 typedef struct MenuNode {
   MenuNodeKind menu_node_kind;
   union {
     BaseMenu *base_menu;
+    BaseAction *base_action;
     Player *player;
     Enemy *enemy;
     Room *room;
@@ -36,10 +54,14 @@ typedef struct MenuNode {
 } MenuNode;
 
 typedef struct Menu {
-  char *name[32];
-  char *description[256];
+  MenuKind menu_kind;
+  char name[32];
+  char description[256];
   int count;
   MenuNode *options[];
 } Menu;
 
+MenuNode *build_menu_node(MenuNodeKind menu_node_kind, void *data);
+void menu_realloc(Menu *menu, int count);
 void display_menu(Menu *menu);
+Menu *parse_player_choice(Player *player, Menu *menu, char *choice);
